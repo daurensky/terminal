1b606abfd2d5215240ba4414260ccafc1d1aea7f
Dauren Kambarov <dkambarov17@gmail.com>
2021-03-05 01:34:55 +0600

    changes

--- sended.php
+++ sended.php
@@ -1,15 +1,34 @@
 <?php
 require_once 'core/blocks/header.php';
+require_once 'core/db.php';
+
+if ($_POST) {
+    $state = (new DB)->query(
+        'INSERT INTO appeals (name, phone, type, message) VALUES (?, ?, ?, ?)',
+        [$_POST['name'], $_POST['phone'], $_POST['type'], $_POST['message']]
+    );
+
+    $state->execute();
+}
 
 ?>
 
 <main class="sended">
     <div class="container">
         <div class="title">
-            <h1>Спасибо за ваше обращение, с вами свяжутся!</h1>
+            <?php if ($_POST) { ?>
+                <h1>Спасибо за ваше обращение, с вами свяжутся!</h1>
+            <?php } else { ?>
+                <h1>Хотите отправить обращение?</h1>
+            <?php } ?>
         </div>
 
-        <a href="/" class="btn">Домой</a>
+        <div class="btns">
+            <?php if (!$_POST) { ?>
+                <a href="/contacts.php#contact-form" class="btn">Отправить обращение</a>
+            <?php } ?>
+            <a href="/" class="btn">Домой</a>
+        </div>
     </div>
 </main>
 
